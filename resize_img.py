00e65ea9dcf8aaa8c0942d8b4fb53d35bf1bbca1
Paulo Kurtz <krtzpm@gmail.com>
2024-11-27 19:47:26 -0300

    tratamento para redimensionar dataset

---
+++ resize_img.py
@@ -0,0 +1,18 @@
+import os
+import cv2
+
+def resize_image(img, size=(640, 640)):
+    return cv2.resize(img, size)
+
+def main():
+    images_dir = './Cropped/Test/'
+    output_images_dir = './Resized/Test/'
+    os.makedirs(output_images_dir, exist_ok=True)
+    for f in os.listdir(images_dir):
+        img_path = os.path.join(images_dir, f)
+        if os.path.isfile(img_path) and f.lower().endswith(('.jpg', '.jpeg', '.png')):
+            img = cv2.imread(img_path)
+            if img is not None:
+                cv2.imwrite(os.path.join(output_images_dir, f), resize_image(img))
+
+if __name__ == '__main__': main()
