25d15a1d6eeec78dabf85e6c10e01d33dc2a4923
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2024-11-27 20:57:54 -0300

    muda path do modelo

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    m = YOLO('./runs/detect/train/weights/best.pt')
+    m = YOLO('./best.pt')
     results = m.predict(source='./Resized/Test/', imgsz=640, conf=0.5, save=True)
