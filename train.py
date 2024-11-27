508eec7f29448cbb55d8c67418d4ae7098ce7cc1
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2024-11-27 20:29:45 -0300

    adiciona modelo yolo8n

---
+++ train.py
@@ -0,0 +1,5 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    m = YOLO('yolo8n.pt')
+    training_results = m.train(data='brain-tumor.yaml', batch=-1, imgsz=640, optimizer='auto', patience=10)
